70a6ff2b3d87dbc23ede19f6639eb3df3a860bae
Anna Sergeeva <annasergeeva29@ya.ru>
2025-05-08 18:04:34 +0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,107 @@
+import React from "react";
+import "./index.css";
+
+const cardData = [
+  {
+    id: 1,
+    title: "Мокка",
+    description: "Развиваем финтех-продукт для международного рынка",
+    date: "24 апреля 2024",
+    imageUrl: "/img-1.jpeg",
+    tags: ["#финтех", "#международный", "#рынок"],
+    archived: false,
+  },
+  {
+    id: 2,
+    title: "Деньги Вперед",
+    description: "Фронтенд и бэкенд для сервиса выплат зарплат по запросу",
+    date: "16 января 2024",
+    imageUrl: "/img-2.jpeg",
+    tags: ["#финансы", "#сервис", "#выплаты"],
+    archived: false,
+  },
+  {
+    id: 3,
+    title: "ResolHR",
+    description: "Помогли HR-tech-стартапу с кастомизацией для VIP-клиентов",
+    date: "10 октября 2023",
+    imageUrl: "/img-3.jpeg",
+    tags: ["#HR", "#кастомизация", "#VIP"],
+    archived: true,
+  },
+  {
+    id: 4,
+    title: "ActivePlatform",
+    description: "Интеграция Adobe и развитие платформы комплексной подписки",
+    date: "10 ноября 2022",
+    imageUrl: "/img-4.jpeg",
+    tags: ["#интеграция", "#платформа", "#подписка"],
+    archived: false,
+  },
+  {
+    id: 5,
+    title: "START",
+    description: "Разработали платформу A/B тестов для стримингового сервиса",
+    date: "22 сентября 2022",
+    imageUrl: "/img-5.jpeg",
+    tags: ["#A/B тесты", "#стриминг", "#платформа"],
+    archived: false,
+  },
+  {
+    id: 6,
+    title: "Mindbox",
+    description:
+      "Поддерживаем редизайн платформы автоматизированного маркетинга",
+    date: "21 сентября 2022",
+    imageUrl: "/img-6.jpeg",
+    tags: ["#маркетинг", "#редизайн", "#автоматизация"],
+    archived: false,
+  },
+];
+
+export default function App() {
+  return <CardContainer />;
+}
+
+function CardContainer() {
+  return (
+    <div className="card-container">
+      {cardData.map((card) => (
+        <Card
+          key={card.id}
+          title={card.title}
+          description={card.description}
+          date={card.date}
+          img={card.imageUrl}
+          tags={card.tags}
+        />
+      ))}
+    </div>
+  );
+}
+function Card(props) {
+  return (
+    <div className="card">
+      <img className="card-image" src={props.img} alt={props.title} />
+      <div className="card-content">
+        <h3 className="card-title">{props.title}</h3>
+        <p className="card-description">{props.description}</p>
+        <span className="card-date">{props.date}</span>
+        <CardTag tags={props.tags} />
+      </div>
+    </div>
+  );
+}
+
+function CardTag(prop) {
+  const tags = prop.tags;
+  return (
+    <div className="card-tags">
+      {tags.map((tag) => (
+        <div key={tag} className="card-tag">
+          {tag}
+        </div>
+      ))}
+    </div>
+  );
+}
